beae9c0ec98d67a529c116941691b288db8d0e9b
Kishan Bhatt <bhattkishan009@gmail.com>
2020-11-04 01:12:45 -0500

    New Proj

---
+++ GoodProject2020/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using GoodProject2020.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace GoodProject2020.Controllers
+{
+    [Route("api")]
+    public class HomeController : ApiController
+    {
+        IRepo _repo;
+        public HomeController(IRepo repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Index()
+        {
+            return _repo.Get();
+        }
+    }
+}
